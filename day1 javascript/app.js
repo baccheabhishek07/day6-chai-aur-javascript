cd38521136a696014343dfbf1db326a12ba61353
Baccheabhishek <baccheabhishek0707@gmail.com>
2024-07-13 19:05:31 +0530

    first commit

---
+++ day1 javascript/app.js
@@ -0,0 +1,47 @@
+// variable declaration
+// Task 1
+var number = 7;
+console.log("varNumber=",number);
+
+// task 2
+let string1="Abhishek";
+console.log("string1=", string1);
+
+// constant declaration
+// task 3
+const boolean1 = true;
+console.log("Boolean1=",boolean1);
+
+// data types
+// task 4
+let number = 7;
+let string2 ="Abhishek Bacche";
+let boolean2 = false ;
+let array =[1,2,3,4,5];
+
+console.log((typeof number));
+console.log((typeof string2));
+console.log((typeof boolean2));
+console.log((typeof array));    /* array is itself trated as a object */
+
+// reassigning variables
+// task 5
+let intialValue="Prasad";
+let initailValue="prem";
+console.log("initiallValue=",intialValue);
+console.log("initialValue=",initailValue);
+
+// understanding const
+// task 6
+const boolean3 = true;
+console.log("Boolean1=",boolean3);
+
+
+
+
+
+
+
+
+
+
